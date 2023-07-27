44068f82e6e49a2b6cabfd39eb1dacfddd434e40
Ankit-saw07 <ankit.1534.kumar@gmail.com>
2023-07-27 09:02:27 +0530

    added admin login and page, also removed menu option from cart icon

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import { Grid, GridItem,Box } from '@chakra-ui/react'
 import { SingleProduct } from "../Pages/SingleProduct";
 import {Link} from "react-router-dom";
+import { useContext } from "react";
+import { cartContext } from "../Context/CartContext";
 
 export const ProductCard = ({
   id,
@@ -16,6 +18,19 @@ export const ProductCard = ({
   catg,
   color,
 }) => {
+
+  const{handleCart}= useContext(cartContext);
+  function addToCart(){
+    handleCart({id,
+      type,
+      icon,
+      details,
+      disc,
+      mrp,
+      off,
+      catg,
+      color});
+  }
   return (
 <Grid templateColumns='repeat(4, 1fr)' gap={4}>
 <Box w={400} letterSpacing={2}>
@@ -30,7 +45,7 @@ export const ProductCard = ({
             <b>₹{mrp}</b>  <Text  display={'inline'}  textDecoration={"line-through"}>₹{disc}</Text>{off}
           </Text>
          <Text>{details}</Text>   
-         <Button >Add to Cart</Button>
+         <Button onClick={addToCart} >Add to Cart</Button>
          {/* onClick={()=>{{handlecart}} */}
          </Box>
       </Flex>
